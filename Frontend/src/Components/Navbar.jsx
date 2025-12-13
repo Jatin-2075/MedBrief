35f92aeabd51e1cab4a87f4612e9d09e6eac745e
jatin <jatin2372005@gmail.com>
2025-12-13 22:54:03 +0530

    ff

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,19 +1,20 @@
 import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import "../Style/navbar.css";
 
 const Navbar = () => {
     return (
-        <>
-            <div className="menu">
+        <header className="topbar">
+            <div className="logo">SmartZen</div>
+
+            <nav className="menu">
                 <NavLink to="/Home" className="item">Home</NavLink>
                 <NavLink to="/Dashboard" className="item">Dashboard</NavLink>
                 <NavLink to="/Upload" className="item">Uploads</NavLink>
-                <NavLink to="/Reports" className="item">Medical Reports</NavLink>
+                <NavLink to="/Reports" className="item">Reports</NavLink>
                 <NavLink to="/SmartHelper" className="item">Smart Helper</NavLink>
                 <NavLink to="/Help" className="item">Help</NavLink>
-            </div>
-        </>
+            </nav>
+        </header>
     );
 };
 
