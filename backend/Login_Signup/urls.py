a286c5dcad6a59b0b8e438749a80252e5bac37ee
Shivam Gami <shivamprakashgami@gmail.com>
2025-12-18 21:43:21 +0530

    pagal bot

--- backend/Login_Signup/urls.py
+++ backend/Login_Signup/urls.py
@@ -4,6 +4,7 @@ from .views import forgot_password
 from .views import reset_password
 
 
+
 urlpatterns = [
     path("signup/", views.Signup, name="signup"),
     path("login/", views.Login, name="login"),
@@ -14,9 +15,5 @@ urlpatterns = [
 
 
     path("Create_Profile/", views.Profile_creation, name="Create_Profile"),
-    path("Show_Profile/", views.Send_Profile, name="Show_Profile"),
-    path("Send_Status/", views.Sent_status, name="Send_Status"),
-    path("Update_status/", views.Profile_done, name="Profile_done"),
+    path("Send_Profile/", views.Send_Profile, name="Send_Profile")
 ]
-
-
